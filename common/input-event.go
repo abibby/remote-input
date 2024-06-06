b5dcdd66a7a9f80662b7efed99c4f3b79e8f7eef
Adam Bibby <adam_bibby@hotmail.com>
2024-06-05 21:18:40 -0400

    mvp

---
+++ common/input-event.go
@@ -0,0 +1,84 @@
+package common
+
+import (
+	"encoding"
+	"encoding/binary"
+	"fmt"
+	"time"
+)
+
+type InputEventType uint16
+
+const (
+	EV_SYN       = InputEventType(0x00)
+	EV_KEY       = InputEventType(0x01)
+	EV_REL       = InputEventType(0x02)
+	EV_ABS       = InputEventType(0x03)
+	EV_MSC       = InputEventType(0x04)
+	EV_SW        = InputEventType(0x05)
+	EV_LED       = InputEventType(0x11)
+	EV_SND       = InputEventType(0x12)
+	EV_REP       = InputEventType(0x14)
+	EV_FF        = InputEventType(0x15)
+	EV_PWR       = InputEventType(0x16)
+	EV_FF_STATUS = InputEventType(0x17)
+	EV_MAX       = InputEventType(0x1f)
+	EV_CNT       = InputEventType((EV_MAX + 1))
+)
+
+func (t InputEventType) String() string {
+	switch t {
+	case EV_SYN:
+		return "EV_SYN"
+	case EV_KEY:
+		return "EV_KEY"
+	case EV_REL:
+		return "EV_REL"
+	case EV_ABS:
+		return "EV_ABS"
+	case EV_MSC:
+		return "EV_MSC"
+	case EV_SW:
+		return "EV_SW"
+	case EV_LED:
+		return "EV_LED"
+	case EV_SND:
+		return "EV_SND"
+	case EV_REP:
+		return "EV_REP"
+	case EV_FF:
+		return "EV_FF"
+	case EV_PWR:
+		return "EV_PWR"
+	case EV_FF_STATUS:
+		return "EV_FF_"
+	case EV_MAX:
+		return "EV_MAX"
+	case EV_CNT:
+		return "EV_CNT"
+	}
+	return fmt.Sprintf("unknown %d", t)
+}
+
+type InputEvent struct {
+	Time      time.Time
+	EventType InputEventType
+	Code      KeyCode
+	Value     int32
+}
+
+var _ encoding.BinaryUnmarshaler = (*InputEvent)(nil)
+
+func (e *InputEvent) UnmarshalBinary(b []byte) error {
+	if len(b) != 24 {
+		return fmt.Errorf("invalid length")
+	}
+	sec := binary.LittleEndian.Uint64(b[0:8])
+	usec := binary.LittleEndian.Uint64(b[8:16])
+	e.Time = time.Unix(int64(sec), int64(usec))
+
+	e.EventType = InputEventType(binary.LittleEndian.Uint16(b[16:18]))
+	e.Code = KeyCode(binary.LittleEndian.Uint16(b[18:20]))
+	e.Value = int32(binary.LittleEndian.Uint32(b[20:]))
+	return nil
+}
--- server/main.go
+++ server/main.go
@@ -5,6 +5,7 @@ import (
 	"net"
 
 	"github.com/abibby/remote-input/common"
+	"github.com/abibby/remote-input/windows"
 )
 
 func main() {
@@ -27,6 +28,55 @@ func main() {
 	}
 }
 
+var keyMap = map[common.KeyCode]windows.VirtualKey{
+	common.KEY_BACKSPACE: windows.VK_BACK,
+	common.KEY_ENTER:     windows.VK_RETURN,
+	common.KEY_TAB:       windows.VK_TAB,
+	common.KEY_CAPSLOCK:  windows.VK_CAPITAL,
+
+	common.KEY_LEFTCTRL:   windows.VK_CONTROL,
+	common.KEY_RIGHTCTRL:  windows.VK_CONTROL,
+	common.KEY_LEFTSHIFT:  windows.VK_SHIFT,
+	common.KEY_RIGHTSHIFT: windows.VK_SHIFT,
+
+	common.KEY_A: windows.VK_A,
+	common.KEY_B: windows.VK_B,
+	common.KEY_C: windows.VK_C,
+	common.KEY_D: windows.VK_D,
+	common.KEY_E: windows.VK_E,
+	common.KEY_F: windows.VK_F,
+	common.KEY_G: windows.VK_G,
+	common.KEY_H: windows.VK_H,
+	common.KEY_I: windows.VK_I,
+	common.KEY_J: windows.VK_J,
+	common.KEY_K: windows.VK_K,
+	common.KEY_L: windows.VK_L,
+	common.KEY_M: windows.VK_M,
+	common.KEY_N: windows.VK_N,
+	common.KEY_O: windows.VK_O,
+	common.KEY_P: windows.VK_P,
+	common.KEY_Q: windows.VK_Q,
+	common.KEY_R: windows.VK_R,
+	common.KEY_S: windows.VK_S,
+	common.KEY_T: windows.VK_T,
+	common.KEY_U: windows.VK_U,
+	common.KEY_V: windows.VK_V,
+	common.KEY_W: windows.VK_W,
+	common.KEY_X: windows.VK_X,
+	common.KEY_Y: windows.VK_Y,
+	common.KEY_Z: windows.VK_Z,
+
+	common.KEY_1: windows.VK_1,
+	common.KEY_2: windows.VK_2,
+	common.KEY_3: windows.VK_3,
+	common.KEY_4: windows.VK_4,
+	common.KEY_5: windows.VK_5,
+	common.KEY_6: windows.VK_6,
+	common.KEY_7: windows.VK_7,
+	common.KEY_8: windows.VK_8,
+	common.KEY_9: windows.VK_9,
+}
+
 func serve(conn net.Conn) {
 	defer conn.Close()
 
@@ -45,6 +95,17 @@ func serve(conn net.Conn) {
 			log.Fatal(err)
 		}
 
-		log.Printf("%v\n", e)
+		if e.EventType == common.EV_KEY {
+			vKey, ok := keyMap[e.Code]
+			if !ok {
+				log.Printf("no map for key code %d", e.Code)
+				continue
+			}
+			if e.Value == 1 {
+				windows.SendInput(vKey, windows.KEYEVENTF_KEYPRESS)
+			} else if e.Value == 0 {
+				windows.SendInput(vKey, windows.KEYEVENTF_KEYUP)
+			}
+		}
 	}
 }
